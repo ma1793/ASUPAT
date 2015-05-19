07429cc4c96becaa10a6274a434f9a3d00cf8db6
ma1793 <ms.varela17@gmail.com>
2015-05-19 10:22:13 -0600

    Estructura del CMS

---
+++ app/components/mision/mision.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../.';?>
+    </head>
+    <body id="home">
+        <div class="ui inverted masthead segment">
+            <div class="ui page grid">
+                <div class="column">
+                    <div class="ui hidden transition information">
+                        <h1 class="ui inverted header" style="text-align-last: right;">
+                            Asociación Unidos por los Animales <br>
+                        </h1>
+                    </div>
+                    <div class="ui large menu">
+                        <div class="ui inverted orange menu">
+                            <div class="header item">ASUPAT</div>
+                            <div class="right menu">
+
+                                <a class="item">Galería</a>
+                                <a class="item">Donaciones</a>
+                                <a class="item">Eventos</a>
+                                <a class="item">Padrinos</a>
+                                <a class="item">Adopciones</a>
+                                <a class="item">Tienda</a>
+                                <a class="item">Acerca de</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="ui vertical segment">
+            <div class="ui very relaxed stackable page grid">
+                <div class="row">
+                    <div class="column">
+                        <h1 class="center aligned ui header">
+                            Misión
+                        </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="ten wide column">
+                        <div class="ui huge label">
+                            <p>TEXTO DE LA Mision</p>
+                        </div>
+                    </div>
+                    <div class="six wide column">
+                        <img class="ui Large circular image" src="../../../assets/images/perroVision.jpg">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      <div class="ui orange inverted footer vertical segment">
+        <div class="ui stackable center aligned page grid">
+        <div class="ui one column center aligned stackable grid">
+          <div class="column">
+            <h5 class="ui inverted header">Contactanos</h5>
+            <addr>
+              Fundacion ASUPAT<br>
+              Turrialba, Costa Rica<br>
+              asupat@gmail.com<br>
+            </addr>
+            <p>(506) 867-5309</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+</div>
+</body>
+
+</html>
