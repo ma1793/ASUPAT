d913f982ac10f0fa523d9ef79e29e6671e205004
ma1793 <ms.varela17@gmail.com>
2015-05-19 22:40:21 -0600

    archivos cms, con includes php

---
+++ app/shared/web/sidebar/sidebarCMS.php
@@ -0,0 +1,23 @@
+<?php
+        echo '   <div class="span3">
+                    <div class="sidebar-nav">
+                        <div class="nav-header" data-toggle="collapse" data-target="#portfolio-menu"><i class="icon-briefcase"></i>Portfolio</div>
+                        <ul id="portfolio-menu" class="nav nav-list collapse in">
+
+                            <li ><a href="listPortfolio.html">List, Add/Remove Project</a></li>
+                            <li ><a href="editPortfolio.html">Edit Project</a></li>
+
+                        </ul>
+
+                        <div class="nav-header" data-toggle="collapse" data-target="#blog-menu"><i class="icon-rss"></i>Blog<span class="label label-info">+1</span></div>
+                        <ul id="blog-menu" class="nav nav-list collapse in">
+                            <li ><a href="listBlog.html">List, Add/Remove Entry</a></li>
+                            <li ><a href="editBlog.html">Edit Blog Entry</a></li>
+                        </ul>
+
+                        <div class="nav-header" data-toggle="collapse" data-target="#accounts-menu"><i class="icon-user"></i>Contact<span class="label label-info">+1</span></div>
+                        <ul id="accounts-menu" class="nav nav-list collapse in">
+                            <li ><a href="editContact.html">Edit Contact Info</a></li>
+                        </ul>
+                    </div>
+                </div>';
