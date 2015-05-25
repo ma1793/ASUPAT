ce55ec01f76eb824a3de6f1681180dbd788e931b
ma1793 <ms.varela17@gmail.com>
2015-05-24 18:43:26 -0600

    Estructura estatica correcta para la mapa sitio

---
+++ app/components/mapaSitio/mapaSitio.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include '../../shared/web/header/header.php'; ?>
+    </head>
+    <body id="home">
+        <?php include '../../shared/web/menu/menu.php'; ?>
+        <div class="ui vertical segment">
+            <div class="ui very relaxed stackable page grid">
+                <div class="row">
+                    <div class="column">
+                        <h1 class="center aligned ui header">
+                            Mapa del Sitio
+                        </h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="ui two column centered grid">
+                        <div class="column">
+                            <h2 class="ui header" style="margin-left: -55px">
+                                <i class="map icon"></i>
+                                <div class="content">
+                                    ASUPAT
+                                </div>
+                            </h2>
+                            <div class="ui bulleted list">
+                                <div class="item">
+                                    <a>Acerca de Nosotros</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Visión</a>
+                                        <a class="item">Misión</a>
+                                    </div>
+                                </div>
+                                <a class="item">Tablero de Adopciones</a>
+                                <a class="item">Padrinos</a>
+                                <a class="item">Donaciones</a>
+                                <div class="item">
+                                    <a>Voluntarios</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Formulario de Ingreso</a>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <a>Actividades</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Descripción de la Actividad</a>
+                                        <a class="item">Eventos</a>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <a>Educación</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Castraciones</a>
+                                        <a class="item">Entrenamiento</a>
+                                        <a class="item">Consejos para tus Mascotas</a>
+                                    </div>
+                                </div>
+                                <div class="item">
+                                    <a>Galería</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Eventos</a>
+                                        <a class="item">Animales Adoptados</a>
+                                        <a class="item">Finales Felices</a>
+                                    </div>
+                                </div>            
+                              
+                                <div class="item">
+                                    <a>Tienda Asupat</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Descripción de productos</a>
+    
+                                    </div>
+                                </div>
+                                
+                                <div class="item">
+                                    <a>Contacto</a>
+                                    <div class="list" style="margin-left:50px" >
+                                        <a class="item">Enlaces de interés</a>
+    
+                                    </div>
+                                </div>
+                                
+                                <a class="item">Feedbak</a>
+                                <a class="item">Preguntas Frecuentes</a>
+                                
+                            </div>
+                            
+                            
+                            
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+   
+
+
+    <?php include '../../shared/web/footer/footer.php'; ?>
+
+</body>
+</html>
