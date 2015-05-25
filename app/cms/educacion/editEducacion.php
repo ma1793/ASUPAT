82fc97548d1fffcdce88e55c963d1c7ccbd8a559
ma1793 <ms.varela17@gmail.com>
2015-05-25 01:10:00 -0600

    Estructura correcta educacion cms

---
+++ app/cms/educacion/editEducacion.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include '../../shared/web/header/headerCMS.php'; ?>
+
+    </head>
+    <body>
+
+        <?php include '../../shared/web/menu/menuCMS.php'; ?>
+
+        <div class="container-fluid">
+
+            <div class="row-fluid">
+                <?php include '../../shared/web/sidebar/sidebarCMS.php'; ?>
+                <div class="span9">
+                    <h1 class="page-title">Editar Información de Contacto</h1>
+                    <div class="btn-toolbar">
+                        <button class="btn btn-primary"><i class="icon-save"></i> Actualizar</button>
+                        
+                    </div>
+                    <div class="well">
+                        
+                        <form id="tab">          
+                                    <label>Calendario Institucional(PDF)</label>
+                                    <input type="file" >
+                                    <p></p>
+                                    <label>Padrinos Actuales(PDF)</label>
+                                    <input type="file" >
+               
+                        </form>
+
+                    </div>
+
+                    <?php include '../../shared/web/modal/modalCMS.php'; ?>
+
+                </div>
+            </div>
+            <?php include '../../shared/web/footer/footerCMS.php'; ?>
+
+
+    </body>
+</html>
+
+
+
