dd87fcb061911b0162b84d6de7a93671ba974a46
ma1793 <ms.varela17@gmail.com>
2015-05-25 00:01:24 -0600

    pdf centrados

--- app/components/educacion/educacion.php
+++ app/components/educacion/educacion.php
@@ -11,23 +11,43 @@
     <!-- Menu -->
     <?php include '../../shared/web/menu/menu.php'; ?>
     
-    <div class="ui teal inverted piled segment">
-        <div class="ui very relaxed stackable page grid">
-            <div class="row">
-                <div class="column">
-                    <h1 class="center aligned ui header">
-                        ASUPAT | Calendario Institucional
-                    </h1>
-                </div>
-            </div>
-        </div>
-        <embed src="Ensayo1.pdf#toolbar=0" width="80%" height="100%">
-        <h4 class="ui horizontal header divider">
-            <i class="circular thumbs up  icon"></i>
-            Actuales Padrinos
-        </h4>
-        <embed src="Ensayo1.pdf#toolbar=0" width="80%" height="100%">
-    </div>
+  
+                    <div class="ui vertical segment">
+                        <div class="ui very relaxed stackable page grid">
+                            <div class="row">
+                                <div class="column">
+                                    <h1 class="center aligned ui header">
+                                        ASUPAT | Calendario Institucional
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="ui two column centered grid">
+                                <div class="column">
+                                    <embed src="Ensayo1.pdf#toolbar=0" width="100%"height="300px!important" >
+                        <h4 class="ui horizontal header divider">
+                            <i class="circular thumbs up  icon"></i>
+                            Actuales Padrinos
+                        </h4>
+                        <embed src="Ensayo1.pdf#toolbar=0" width="100%" height="300px!important">
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+
+                
+
+
+
+
+    <?php include '../../shared/web/footer/footer.php'; ?>
+
+    
+   
     
 </body>
 </html>
