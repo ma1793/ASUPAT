056e87e0294dba59a73d244f44610f67a5bce299
ma1793 <ms.varela17@gmail.com>
2015-05-25 00:06:23 -0600

    pdf centrados

--- app/components/educacion/educacion.php
+++ app/components/educacion/educacion.php
@@ -1,53 +1,38 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>ASUPAT | Actividades</title>
-    
-    <?php include '../../shared/web/header/header.php'; ?>
-    <script src="actividades.js"></script>
+    <head>
+       
 
-</head>
-<body id="home">
-    <!-- Menu -->
-    <?php include '../../shared/web/menu/menu.php'; ?>
-    
-  
-                    <div class="ui vertical segment">
-                        <div class="ui very relaxed stackable page grid">
-                            <div class="row">
-                                <div class="column">
-                                    <h1 class="center aligned ui header">
-                                        ASUPAT | Calendario Institucional
-                                    </h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="ui two column centered grid">
-                                <div class="column">
-                                    <embed src="Ensayo1.pdf#toolbar=0" width="100%"height="300px!important" >
+        <?php include '../../shared/web/header/header.php'; ?>
+        <script src="actividades.js"></script>
+
+    </head>
+    <body id="home">
+        <!-- Menu -->
+        <?php include '../../shared/web/menu/menu.php'; ?>
+        <div class="ui vertical segment">
+            <div class="ui very relaxed stackable page grid">
+                <div class="row">
+                    <div class="column">
+                        <h1 class="center aligned ui header">
+                            Calendario Institucional
+                        </h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="ui two column centered grid">
+                    <div class="column">
+                        <embed src="Ensayo1.pdf#toolbar=0" width="100%"height="300px!important" >
                         <h4 class="ui horizontal header divider">
                             <i class="circular thumbs up  icon"></i>
                             Actuales Padrinos
                         </h4>
                         <embed src="Ensayo1.pdf#toolbar=0" width="100%" height="300px!important">
-
-                                </div>
-                            </div>
-                        </div>
-                        
-                        
                     </div>
-
-                
-
-
-
-
-    <?php include '../../shared/web/footer/footer.php'; ?>
-
-    
-   
-    
-</body>
+                </div>
+            </div>
+        </div>
+        <?php include '../../shared/web/footer/footer.php'; ?>
+    </body>
 </html>
